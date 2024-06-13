00c4d6a813083fe644a60912589c46aff26ad4c4
Ing. Jordan García <52794915+JordanGarciaDev@users.noreply.github.com>
2024-06-13 11:29:54 -0500

    Add files via upload

---
+++ PHPWebHookBasico.php
@@ -0,0 +1,41 @@
+<?php
+    const TOKEN_ANDERCODE = "JORDANDEVPHPAPIMETA";
+    const WEBHOOK_URL = "https://jordangarciadev.com/webhook.php";
+
+    function recibirMensajes($req, $res) {
+        
+        try {
+            
+            $entry = $req['entry'][0];
+            $changes = $entry['changes'][0];
+            $value = $changes['value'];
+            $mensaje = $value['messages'][0];
+            
+            $comentario = $mensaje['text']['body'];
+            $numero = $mensaje['from'];
+            
+    
+            $res->header('Content-Type: application/json');
+            $res->status(200)->send(json_encode(['message' => 'EVENT_RECEIVED']));
+
+        } catch (Exception $e) {
+            $res->header('Content-Type: application/json');
+            $res->status(200)->send(json_encode(['message' => 'EVENT_RECEIVED']));
+        }
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD']==='POST'){
+        $input = file_get_contents('php://input');
+        $data = json_decode($input,true);
+
+        recibirMensajes($data,http_response_code());
+        
+    }else if($_SERVER['REQUEST_METHOD']==='GET'){
+        if(isset($_GET['hub_mode']) && isset($_GET['hub_verify_token']) && isset($_GET['hub_challenge']) && $_GET['hub_mode'] === 'subscribe' && $_GET['hub_verify_token'] === TOKEN_ANDERCODE){
+            echo $_GET['hub_challenge'];
+        }else{
+            http_response_code(403);
+        }
+    }
+?>
